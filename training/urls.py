2fb7716fcdd58b8978a47d88c5846386cd4888b4
karanthapa <karanmagar078580@gmail.com>
2026-04-16 21:44:30 +0930

    updated templates and urls

--- training/urls.py
+++ training/urls.py
@@ -6,7 +6,12 @@ app_name = "training"
 
 urlpatterns = [
     path("", views.ProgramListView.as_view(), name="program_list"),
-    path("<int:pk>/", views.ProgramDetailView.as_view(), name="program_detail"),
+    path("programs/<slug:slug>/", views.ProgramDetailView.as_view(), name="program_detail"),
+
     path("regions/", views.RegionListView.as_view(), name="region_list"),
+    path("regions/<slug:slug>/", views.RegionDetailView.as_view(), name="region_detail"),
+
     path("categories/", views.CategoryListView.as_view(), name="category_list"),
+    path("categories/<str:name>/", views.CategoryDetailView.as_view(), name="category_detail"),
+
 ]
