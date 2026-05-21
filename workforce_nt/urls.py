d965dc645ea1fec7d7483c004c58f8f725aad909
karanthapa <karanmagar078580@gmail.com>
2026-05-21 20:52:39 +0930

    url update

--- workforce_nt/urls.py
+++ workforce_nt/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("", include("training.urls", namespace="training")),
+    path("", RedirectView.as_view(url="/", permanent=False))
 ]
